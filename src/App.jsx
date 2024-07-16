e6863fbabbf849c6f4aecbcb28edf397c34ad29d
Vituin <124085647+Vituin@users.noreply.github.com>
2024-07-16 17:42:31 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
-import "./App.css";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox";
+import foods from "./foods.json";
+import Search from "./components/Search";
+import AddFoodForm from "./components/AddFoodForm";
+import { Row, Divider, Button } from "antd";
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+  const [foodItems, setFoodItems] = useState(foods);
+  const [searchValue, setSearchValue] = useState("");
+
+  const displayForm = () => setShowForm(!showForm);
+
+  const addNewFood = (newFood) => {
+    const newFoodItems = [...foodItems, newFood];
+    setFoodItems(newFoodItems);
+  };
+
+  const deleteItem = (name) => {
+    const deletedItems = foodItems.filter((food) => {
+      return food.name !== name;
+    });
+    setFoodItems(deletedItems);
+  };
+
+  const filteredFoodItems = foodItems.filter((el) => {
+    return el.name.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      {showForm && <AddFoodForm addNewFood={addNewFood} />}
+
+      <Button onClick={displayForm}>
+        {showForm ? "Hide Form" : "Add New Food"}
+      </Button>
+
+      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+
+      <Divider>Food List</Divider>
+
+      {filteredFoodItems.length > 0 ? (
+        <Row style={{ width: "100%", justifyContent: "center" }}>
+          {filteredFoodItems.map((food) => {
+            return <FoodBox food={food} deleteItem={deleteItem} />;
+          })}
+        </Row>
+      ) : (
+        <h2>Nothing to see here</h2>
+      )}
     </div>
   );
 }
